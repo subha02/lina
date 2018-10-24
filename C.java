49a1693ded31688feba010ec57c2ef0450378c65
subha02 <44401431+subha02@users.noreply.github.com>
2018-10-24 05:47:41 -0700

    Add files via upload

---
+++ C.java
@@ -0,0 +1,15 @@
+class C 
+{
+	public static void main(String[] args) 
+	{
+		int i = 0;
+		int j = ++i + i + ++i + i ;
+		i=0;
+		int k = ++i + i + i ++ + i;
+		System.out.println(i);
+		system.out.println(j);
+		system.out.println(k);
+	}
+		
+	}
+
